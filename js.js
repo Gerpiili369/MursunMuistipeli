2f2be55040bc4d4893ee5b1c06a52f6c49a19277
Gerpiili369 <elektromiikka@hotmail.com>
2017-08-18 11:58:11 +0300

    Multiplayer and cleanup

--- js.js
+++ js.js
@@ -2,6 +2,9 @@ var first="";
 var second="";
 var firstCell="";
 var ready=1;
+var player=1;
+var p1scr=0;
+var p2scr=0;
 
 window.setInterval(function () {
     console.log("first: "+first+", second: "+second+", firstCell: "+firstCell+", ready: "+ready);
@@ -17,8 +20,6 @@ function ChangeColor(cellid) {
             document.getElementById(cellid).setAttribute("bgcolor","red");
             firstCell=cellid;
             ready=1;
-            return;
-
         } else {
             second=document.getElementById(cellid).innerHTML;
             document.getElementById(cellid).setAttribute("bgcolor","red");
@@ -26,20 +27,28 @@ function ChangeColor(cellid) {
             if(first===second) {
                 document.getElementById(cellid).setAttribute("bgcolor","green");
                 document.getElementById(firstCell).setAttribute("bgcolor","green");
-                retry();
-                return;                      
+                
+                if (player===1) {
+                    ++p1scr
+                } else {
+                    ++p2scr
+                }
+
+            retry();
             } else {
                 window.setTimeout(function () {
                     document.getElementById(cellid).setAttribute("bgcolor","black");
                     document.getElementById(firstCell).setAttribute("bgcolor","black");
+                    if (player===1) {
+                        player=2
+                    } else {
+                        player=1
+                    }
                     retry();
-                    return;
                 }, 800);
             }
         }
-    } else {
-        return
-    } 
+    }
 }
 
 function retry() {
@@ -48,4 +57,5 @@ function retry() {
     firstCell=""
     ready=1;
 
+    document.getElementById("score").innerHTML="p1 score: "+p1scr+", p2 score: "+p2scr;
 }
